e60d76ee36ad11ecdbb0bf9e1d02846619342d43
Tyler Neylon <tylerneylon@gmail.com>
2012-05-13 15:57:14 -0700

    print_str now works.
    Input is not ready yet.

--- display.py
+++ display.py
@@ -12,6 +12,7 @@ import time
 
 # This is useful for debugging.
 f = open('debug_log', 'w')
+term_height = 10
 
 class display():
   # Set up the curses environment.
@@ -22,15 +23,23 @@ class display():
     curses.cbreak()
     self.win.border()
     h, w = self.win.getmaxyx()
+
     # Set the title.
     title = " BattleBoat! "
     self.win.addstr(0, ((w - len(title)) // 2), title)
+
     # Set up the terminal portion of the screen as the
     # lower 10 lines.
-    self.term = self.win.subwin(h - 10, 0)
+    self.term = self.win.subwin(h - term_height, 0)
+    self.term.setscrreg(1, term_height - 2)
+    self.term.scrollok(True)
     self.term.border()
     self.win.refresh()
     self.term.refresh()
+
+    # Keep track of our current position in the terminal.
+    self.termline = 1
+
     f.write('__init__ done\n')
 
   def __del__(self):
@@ -42,11 +51,27 @@ class display():
 
   def print_str(self, s):
     f.write('print_str\n')
-    self.term.addstr(1, 1, s)
+    self.term.addstr(self.termline, 1, s)
+    self.termline += 1
+    if self.termline == term_height - 1:
+      try:
+        self.term.scroll(1)
+        self.term.border()
+      except Exception, e:
+        f.write('%s\n' % `e`)
+      self.termline = term_height - 2 
+    self.term.move(self.termline, 1)
     self.term.refresh()
+
+  def input_str(self):
+    pass
   
 
 if __name__ == '__main__':
   d = display()
-  d.print_str('hello')
-  time.sleep(1)
+  slist = ['hi', 'there', 'how', 'are', 'you', 'fine', 'thanks', 'nice', 'of', 'you', 'to', 'ask']
+  for s in slist:
+    d.print_str(s)
+    d.win.getch()
+  time.sleep(2)
+
